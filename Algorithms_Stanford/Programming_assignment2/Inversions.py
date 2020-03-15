028d9ece796d3b0864a378408952bf6db8e0eff6
Jon <juxue54@gmail.com>
2020-03-15 09:03:57 +0800

    simplified inversions

--- Algorithms_Stanford/Programming_assignment2/Inversions.py
+++ Algorithms_Stanford/Programming_assignment2/Inversions.py
@@ -7,45 +7,32 @@ def read(filename='IntegerArray.txt'):
     return list(map(int,s))
 
 def count(a):
-    aux=[x for x in a]
-    return counting(a,aux,0,len(a)-1)
+    return counting(a,0,len(a)-1)
 
-def counting(a, aux, low, high):
+def counting(a, low, high):
     if high<=low:
         return 0
     mid=(high-low)//2+low
-    x=counting(a,aux,low,mid)
-    y=counting(a,aux,mid+1,high)
+    x=counting(a,low,mid)
+    y=counting(a,mid+1,high)
     if a[mid+1]>a[mid]:
         return x+y
-    z=countsplit(a,aux,low,mid,high)
+    z=countsplit(a,low,mid,high)
     return x+y+z
 
-def countsplit(a,aux,low,mid,high):
-    aux[low:high+1]=[i for i in a[low:high+1]]
-    i,j,num,lr=low,mid+1,0,0
+def countsplit(a,low,mid,high):
+    aux=[i for i in a[low:high+1]]
+    i,j,num,lr=0,mid-low+1,0,0
     for k in range(low,high+1):
-        if i>mid:
-            a[k]=aux[j]
-            j+=1
-        elif j>high:
-            a[k]=aux[i]
-            i+=1
-            num+=lr
+        if i>mid-low:
+            a[k],j=aux[j],j+1
+        elif j>high-low:
+            a[k],i,num=aux[i],i+1,num+lr
         elif aux[j]<aux[i]:
-            a[k]=aux[j]
-            j+=1
-            lr+=1
+            a[k],j,lr=aux[j],j+1,lr+1
         else:
-            a[k]=aux[i]
-            i+=1
-            num+=lr
+            a[k],i,num=aux[i],i+1,num+lr
     return num
-
-def exch(a,i,j):
-    temp=a[i]
-    a[i]=a[j]
-    a[j]=temp
 
 def client():
     data=read('IntegerArray.txt')
